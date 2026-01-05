5bfd707803b702e3116ca6137c0755441d5d9908
EliLentz <feedapps.royal@gmail.com>
2026-01-05 06:21:32 +0100

    Using OnCollisionEnter()

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,28 +2,26 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] private float moveSpeed = 10f;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PrintInstructions();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         MovePlayer();
     }
 
-    void PrintInstructions()
+    private static void PrintInstructions()
     {
         Debug.Log("Welcome to the game!");
         Debug.Log("Move using arrow keys or wasd");
         Debug.Log("Don't bump into objects!");
     }
 
-    void MovePlayer()
+    private void MovePlayer()
     {
         var xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         const float yValue = 0f;
